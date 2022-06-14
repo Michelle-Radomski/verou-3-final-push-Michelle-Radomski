d096d377ce12168e69809c6da4638ae76fb62993
Michelle Radomski <michi.radomski@hotmail.com>
2022-06-14 17:29:53 +0200

    Make basic navbar with icons + useState

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,38 @@
 import React from "react";
+import {useState} from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+    const toggleMenu = () => {
+        setClick(!click);
+    }
+    const closeMobileMenu = () => {
+        setClick(false);
+    }
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to="/">
-                        DYSMANTLE
+                    <Link to="/" className="navbar-logo">
+                        DYSMANTLE <i className="fa fa-tree"></i> 
                     </Link>
                 </div>
+                <div className="menu-icon" onClick={toggleMenu}>
+                    <i className={click ? "fa fa-times" : "fa fa-bars" }></i>
+                </div>
+                <ul className={click ? "nav-menu active" : "nav-menu"}>
+                    <li className="nav-item">
+                        <Link to='/' className="nav-links" onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/weapons' className="nav-links" onClick={closeMobileMenu}>
+                            Weapons
+                        </Link>
+                    </li>
+                </ul>
             </nav>
         </>
     ); 
