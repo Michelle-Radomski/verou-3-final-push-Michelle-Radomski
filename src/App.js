423031c9ba5f9c06420047bac55096df55e54ac6
Michelle Radomski <michi.radomski@hotmail.com>
2022-06-14 14:46:40 +0200

    Install + use basic router for navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-function App() {
-    return <div>Hello!</div>;
-}
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = () => {
+    return (
+        <>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" />
+                </Routes>
+            </Router>
+        </>
+    );
+};
 
 export default App;
