c9c70b362d2239b2f18e380dd5e20bc119e6ce6b
Michelle Radomski <michi.radomski@hotmail.com>
2022-06-15 09:19:10 +0200

    Add dysmantle logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {useState} from "react";
 import { Link } from "react-router-dom";
+import dysmantleLogo from "../img/Dysmantle-logo.png";
 
 function Navbar() {
     const [click, setClick] = useState(false);
@@ -15,24 +16,25 @@ function Navbar() {
             <nav className="navbar">
                 <div className="navbar-container">
                     <Link to="/" className="navbar-logo">
-                        DYSMANTLE <i className="fa fa-tree"></i> 
+                        {/* DYSMANTLE <i className="fa fa-tree"></i> */}
+                        <img src={dysmantleLogo} alt="Dysmantle logo." /> 
                     </Link>
+                    <div className="menu-icon" onClick={toggleMenu}>
+                        <i className={click ? "fa fa-times" : "fa fa-bars" }></i>
+                    </div>
+                    <ul className={click ? "nav-menu active" : "nav-menu"}>
+                        <li className="nav-item">
+                            <Link to='/' className="nav-links" onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/weapons' className="nav-links" onClick={closeMobileMenu}>
+                                Weapons
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
-                <div className="menu-icon" onClick={toggleMenu}>
-                    <i className={click ? "fa fa-times" : "fa fa-bars" }></i>
-                </div>
-                <ul className={click ? "nav-menu active" : "nav-menu"}>
-                    <li className="nav-item">
-                        <Link to='/' className="nav-links" onClick={closeMobileMenu}>
-                            Home
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to='/weapons' className="nav-links" onClick={closeMobileMenu}>
-                            Weapons
-                        </Link>
-                    </li>
-                </ul>
             </nav>
         </>
     ); 
