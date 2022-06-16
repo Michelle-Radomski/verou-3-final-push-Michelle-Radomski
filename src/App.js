ce177fbbb7c290cad448f5778a96895f0b188891
Michelle Radomski <michi.radomski@hotmail.com>
2022-06-16 12:04:57 +0200

    Use router to navigate to homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -8,7 +9,7 @@ const App = () => {
             <Router>
                 <Navbar />
                 <Routes>
-                    <Route path="/" />
+                    <Route path="/" element={<Home />} />
                 </Routes>
             </Router>
         </>
