73ffd78faaac235292a92cb054021891973d1514
Michelle Radomski <michi.radomski@hotmail.com>
2022-06-16 15:57:57 +0200

    Add Button component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import dysmantleLogo from "../images/Dysmantle-logo.png";
 import platforms from "../images/platforms.png";
+import Button from "../components/Button";
 
 function Hero() {
   return (
     <section className="hero-container">
         <img src={platforms} alt="Available platforms." className="platforms"/>
         <img src={dysmantleLogo} alt="Dysmantle logo." className="logo"/>
+        <Button text="More info"/>
     </section>
   )
 }
