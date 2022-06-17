9cf8bf1dc9007cdee7c294a3cf0ff7c8d95a21b5
Michelle Radomski <michi.radomski@hotmail.com>
2022-06-17 12:23:22 +0200

    Change class name

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,7 +5,7 @@ function Home() {
     return (
         <main>
             <Hero />
-            <section className="homepage-info-container">
+            <section className="info-container">
                 <p>DYSMANTLE is a post-apocalyptic game with RPG elements, where players can break down almost every object (except those which are needed for interaction).
                 Basically, players come from the safety of a vault on an island full with dangerous creatures.
                 </p>
