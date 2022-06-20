d1d0ea77b65e01fd23d5718fd25145adaf2ab421
Michelle Radomski <michi.radomski@hotmail.com>
2022-06-20 13:47:15 +0200

    Create level 1 weapons

---
+++ src/components/Level1.js
@@ -0,0 +1,89 @@
+// import Crowbar from "./images/crowbar";
+
+export default [
+    {
+        // src: Crowbar,
+        // alt: "Crowbar",
+        title: "Crowbar",
+        description:
+            "Can be used for defense and to break lightweight objects.",
+        damage: 12,
+        powerAttackDamage: "+60%",
+    },
+    {
+        // src: Machete,
+        // alt: "Machete",
+        title: "Machete",
+        description:
+            "A weapon with a broad blade usable both in combat and gardening.",
+        damage: 35,
+        powerAttackDamage: "+30%",
+        criticalHitDamage: "+60%",
+    },
+    {
+        // src: BaseballBat,
+        // alt: "Baseball Bat",
+        title: "Baseball Bat",
+        description:
+            "Originally used to hit balls very far, but should double as a handy weapon.",
+        damage: 30,
+        powerAttackDamage: "+55%",
+    },
+    {
+        // src: Katana,
+        // alt: "Katana",
+        title: "Katana",
+        damage: 20,
+        extraTargets: "+2",
+        meleeDamage: "+33.3%",
+    },
+    {
+        // src: Axe,
+        // alt: "Axe",
+        title: "Axe",
+        damage: 31,
+        powerAttackDamage: "+100%",
+        criticalHitDamage: "+20%",
+    },
+    {
+        // src: Hunting Rifle,
+        // alt: "Hunting Rifle",
+        title: "Hunting Rifle",
+        damage: 84,
+        uses: 5,
+    },
+    {
+        // src: Sledgehammer,
+        // alt: "Sledgehammer",
+        title: "Sledgehammer",
+        damage: 56,
+        powerAttackDamage: "+100%",
+        stunChance: "+20%",
+    },
+    {
+        // src: Riot Shield,
+        // alt: "Riot Shield",
+        title: "Riot Shield",
+        damage: 20,
+        powerAttackDamage: "+200%",
+        hitPoints: "+100",
+        knockbackStrength: "+160",
+    },
+    {
+        // src: Khopesh,
+        // alt: "Khopesh",
+        title: "Khopesh",
+        damage: 80,
+        powerAttackDamage: "+60%",
+        criticalHitDamage: "+33.3%",
+    },
+    {
+        // src: Power Fist,
+        // alt: "Power Fist",
+        title: "Power Fist",
+        damage: 80,
+        powerAttackDamage: "+80%",
+        knockbackStrength: "+200",
+        extraTargets: "+2",
+    },
+];
