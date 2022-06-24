697b431082d8261bb17e0b32661157a6c10fb997
Michelle Radomski <michi.radomski@hotmail.com>
2022-06-24 17:35:42 +0200

    Add click event to toggle weapon info

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -42,31 +42,41 @@ function CardItem({weapon}) {
         )
     }
 
+    const [showUpgrades, setShowUpgrades] = useState(false);
+
+    const toggleWeaponInfo = () => {
+        setShowUpgrades(!showUpgrades)
+    }
+
         return (
         <article className="weapon-info-container">
             <div className="card-image-container">
                 <img src={weapon.src} alt={weapon.alt} className="card-image" />
             </div>
             <div className="weapon-info-content">
-                <h3>{weapon.title}</h3>
-                <p>{weapon.description}</p>
-                <ul className="weapon-info-list">
-                {weaponInfo.damage ? <li>Damage: {weaponInfo.damage}</li> : ""}
-                {weaponInfo.powerAttackDamage ? <li>Power Attack Damage: {weaponInfo.powerAttackDamage}</li> : ""}
-                {weaponInfo.criticalHitDamage ? <li>Critical Hit Damage: {weaponInfo.criticalHitDamage}</li> : ""}
-                {weaponInfo.knockbackStrength ? <li>Knockback Strength: {weaponInfo.knockbackStrength}</li> : ""}
-                {weaponInfo.extraTargets ? <li>Extra Targets: {weaponInfo.extraTargets}</li> : ""}
-                {weaponInfo.meleeDamage ? <li>Melee Damage: {weaponInfo.meleeDamage}</li> : ""}
-                {weaponInfo.uses ? <li>Uses: {weaponInfo.uses}</li> : ""}
-                {weaponInfo.stunChance ? <li>Stun Chance: {weaponInfo.stunChance}</li> : ""}
-                {weaponInfo.hitPoints ? <li>Hit Points: {weaponInfo.hitPoints}</li> : ""}
-                </ul>
-                <Button className="level-button button" text="level 0" onClick={clearState} />
-                <Button className="level-button button" text="level 1" onClick={upgradeLevel1} />
-                <Button className="level-button button" text="level 2" onClick={upgradeLevel2} />
-                <Button className="level-button button" text="level 3" onClick={upgradeLevel3} />
-                <Button className="level-button button" text="level 4" onClick={upgradeLevel4} />
-                <Button className="level-button button" text="level 5" onClick={upgradeLevel5} />
+                <h3 className="title" onClick={toggleWeaponInfo}>{weapon.title}</h3>
+                {showUpgrades &&
+                <div>
+                    <p>{weapon.description}</p>
+                    <ul className="weapon-info-list">
+                    {weaponInfo.damage ? <li>Damage: {weaponInfo.damage}</li> : ""}
+                    {weaponInfo.powerAttackDamage ? <li>Power Attack Damage: {weaponInfo.powerAttackDamage}</li> : ""}
+                    {weaponInfo.criticalHitDamage ? <li>Critical Hit Damage: {weaponInfo.criticalHitDamage}</li> : ""}
+                    {weaponInfo.knockbackStrength ? <li>Knockback Strength: {weaponInfo.knockbackStrength}</li> : ""}
+                    {weaponInfo.extraTargets ? <li>Extra Targets: {weaponInfo.extraTargets}</li> : ""}
+                    {weaponInfo.meleeDamage ? <li>Melee Damage: {weaponInfo.meleeDamage}</li> : ""}
+                    {weaponInfo.uses ? <li>Uses: {weaponInfo.uses}</li> : ""}
+                    {weaponInfo.stunChance ? <li>Stun Chance: {weaponInfo.stunChance}</li> : ""}
+                    {weaponInfo.hitPoints ? <li>Hit Points: {weaponInfo.hitPoints}</li> : ""}
+                    </ul>
+                    <Button className="level-button button" text="level 0" onClick={clearState} />
+                    <Button className="level-button button" text="level 1" onClick={upgradeLevel1} />
+                    <Button className="level-button button" text="level 2" onClick={upgradeLevel2} />
+                    <Button className="level-button button" text="level 3" onClick={upgradeLevel3} />
+                    <Button className="level-button button" text="level 4" onClick={upgradeLevel4} />
+                    <Button className="level-button button" text="level 5" onClick={upgradeLevel5} />
+                </div>
+                }
             </div>
         </article>
         )
