4aa0b7075f63b69545c06271c67053a8af08e199
Michelle Radomski <michi.radomski@hotmail.com>
2022-06-24 13:03:52 +0200

    Add className as prop for Button component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React from "react";
 import dysmantleLogo from "../images/Dysmantle-logo.png";
 import platforms from "../images/platforms.png";
 import Button from "../components/Button";
 
 function Hero() {
-  return (
-    <section className="hero-container">
-        <img src={platforms} alt="Available platforms." className="platforms"/>
-        <img src={dysmantleLogo} alt="Dysmantle logo." className="logo"/>
-        <Button text="More info"/>
-    </section>
-  )
+    return (
+        <section className="hero-container">
+            <img
+                src={platforms}
+                alt="Available platforms."
+                className="platforms"
+            />
+            <img src={dysmantleLogo} alt="Dysmantle logo." className="logo" />
+            <Button text="More info" className="button" />
+        </section>
+    );
 }
 
-export default Hero
+export default Hero;
