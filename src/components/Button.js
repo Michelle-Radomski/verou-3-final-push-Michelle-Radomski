5a2d70e34e328bbacb69d97c78849881522bbc63
Michelle Radomski <michi.radomski@hotmail.com>
2022-06-24 08:49:43 +0200

    Make Button component more flexible

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Button(props) {
-    return <button className="button">{props.text}</button>;
+    return (
+        <button className={props.className} onClick={props.onClick}>
+            {props.text}
+        </button>
+    );
 }
 
 export default Button;
